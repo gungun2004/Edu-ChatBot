b915672d3107013ae174849d9ad8ecff78f68a59
Gunjan <gunjanaggarwal278@gmail.com>
2025-06-17 21:53:19 +0530

    Code Updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,17 @@
 
-// const { Pool } = require('pg');
-// require('dotenv').config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-// pool.connect()
-//   .then(client => {
-//     console.log('Connected to database successfully!');
-//     client.release();
-//   })
-//   .catch(err => console.error('Connection error', err.stack));
+pool.connect()
+  .then(client => {
+    console.log('Connected to database successfully!');
+    client.release();
+  })
+  .catch(err => console.error('Connection error', err.stack));
 
 
-// module.exports = pool;
+module.exports = pool;
